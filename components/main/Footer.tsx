bca7ff667e79d23f9c63ddc1e05dcf5676cf1cf7
thivviyan98 <thivviyan1998@gmail.com>
2023-10-27 22:05:16 +0530

    major updates

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -45,16 +45,16 @@ const Footer = () => {
             </p>
           </div>
           <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-            <div className="font-bold text-[16px]">About</div>
+            <div className="font-bold text-[16px]">About US</div>
             <p className="flex flex-row items-center my-[15px] cursor-pointer">
               <span className="text-[15px] ml-[6px]">Become Sponsor</span>
             </p>
             <p className="flex flex-row items-center my-[15px] cursor-pointer">
-              <span className="text-[15px] ml-[6px]">Learning about me</span>
+              <span className="text-[15px] ml-[6px]">Learning MORE</span>
             </p>
             <p className="flex flex-row items-center my-[15px] cursor-pointer">
               <span className="text-[15px] ml-[6px]">
-                mifwebchain@gmail.com
+                SILICONSINSTITUTE@gmail.com
               </span>
             </p>
           </div>
